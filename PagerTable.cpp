f6e6cffe577a3b143df3e4b1f304a3efd44545e2
Ryan Le <YOUR_GITHUB_EMAIL>
2026-07-15 11:44:32 -0700

    Migrate PagerTable to platform mutex abstraction

--- PagerTable.cpp
+++ PagerTable.cpp
@@ -1,44 +1,52 @@
 /* 
+/*
  * File:   PagerTable.cpp
- * Author: Adam Hickerson
- * 
- * Created on May 6, 2013, 7:55 AM
+ * Author: Ryan Quang Le
+ *
+ * Created on 07/15/2026
  */
 
-#include <ios>
 #include "PagerTable.h"
+#include "platform/thread/PlatformLockGuard.h"
 
-PagerTable::PagerTable() {
+PagerTable::PagerTable()
+{
 }
 
-void PagerTable::AddPager(PagerTableEntry entry){
-    pthread_mutex_lock(&tableLock);
+PagerTable::~PagerTable()
+{
+}
+
+void PagerTable::AddPager(
+    const PagerTableEntry& entry
+)
+{
+    cigorn::PlatformLockGuard lock(tableLock);
     pagerTable.push_back(entry);
-    pthread_mutex_unlock(&tableLock);
 }
 
-void PagerTable::ClearAll(){
-    pthread_mutex_lock(&tableLock);
+void PagerTable::ClearAll()
+{
+    cigorn::PlatformLockGuard lock(tableLock);
     pagerTable.clear();
-    pthread_mutex_unlock(&tableLock);
 }
 
-PagerTableEntry PagerTable::GetPager(int pagerNumber){
-    pthread_mutex_lock(&tableLock);
-    for(vector<PagerTableEntry>::iterator it = pagerTable.begin(); it != pagerTable.end(); it++){
-        if(it->pagerNumber == pagerNumber){
-            pthread_mutex_unlock(&tableLock);
-            return (*it);
+PagerTableEntry PagerTable::GetPager(
+    int pagerNumber
+)
+{
+    cigorn::PlatformLockGuard lock(tableLock);
+
+    for (const PagerTableEntry& entry : pagerTable)
+    {
+        if (entry.pagerNumber == pagerNumber)
+        {
+            return entry;
         }
     }
-    pthread_mutex_unlock(&tableLock);
-    
-    // No pager found
-    PagerTableEntry badPager;
+
+    PagerTableEntry badPager{};
     badPager.capCode = RESULT_NO_PAGER;
+
     return badPager;
 }
-
-PagerTable::~PagerTable() {
-}
-
--- PagerTable.h
+++ PagerTable.h
@@ -1,41 +1,43 @@
-/* 
+/*
  * File:   PagerTable.h
- * Author: Adam Hickerson
+ * Author: Ryan Quang Le
  *
- * Created on May 6, 2013, 7:55 AM
+ * Created on 07/15/2026
  */
-#include <vector>
-#include <string>
-
-using namespace std;
 
 #ifndef PAGERTABLE_H
-#define	PAGERTABLE_H
+#define PAGERTABLE_H
 
-struct PagerTableEntry{
+#include "platform/thread/PlatformMutex.h"
+
+#include <string>
+#include <vector>
+
+struct PagerTableEntry
+{
     int pagerNumber;
-    string pageDataType;
+    std::string pageDataType;
     int capCode;
-    string otaProtocol;
+    std::string otaProtocol;
     bool isGroup;
     bool isActive;
 };
 
-class PagerTable {
+class PagerTable
+{
 public:
     static const int RESULT_NO_PAGER = -1;
-    
+
     PagerTable();
     virtual ~PagerTable();
 
-    void AddPager(PagerTableEntry entry);
+    void AddPager(const PagerTableEntry& entry);
     void ClearAll();
     PagerTableEntry GetPager(int pagerNumber);
-    
+
 private:
-    vector<PagerTableEntry> pagerTable;
-    pthread_mutex_t tableLock;
+    std::vector<PagerTableEntry> pagerTable;
+    cigorn::PlatformMutex tableLock;
 };
 
-#endif	/* PAGERTABLE_H */
-
+#endif
